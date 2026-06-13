b1d49dcf36e23b9d6c63e92a3fa68516e6448d18
Epic-Mushroom <malroop1@outlook.com>
2026-06-13 16:54:42 -0700

    init node.js

--- js/game.js
+++ js/game.js
@@ -1,13 +1,14 @@
 class Player {
     // has a username, a game that it is connected to, number of lives,
     // current timer value, alphabet matched to the game's alphabet rule (make sure not to update after getting extra life),
-    // if it's the player's turn
+    // if it's the player's turn, if the player is alive
 
     constructor(username, game /* Game */) {
         this.username = username;
         this.game = game;
 
         this.isPlayerTurn = false;
+        this.isPlayerAlive = true;
         this.currentLifeCount = game.startingLives;
         this.currentTimerValue = game.maxTimerLength; // could add variation depending on substring rarity
         this.currentAlphabet = new Set();
@@ -22,7 +23,7 @@ class Game {
     // has an array of players, set that is the dictionary of words,
     // dictionary of substrings mapped to count, dictionary of words already done (key: word, value: player), 
     // room code, alphabet rule (set of letters) for generating extra lives (later, for gd, may want to use demon diffs instead)
-    // max timer length, maxPlayers, starting life count, current round number
+    // max timer length, maxPlayers, starting life count, current round number, current player turn
 
     constructor(roomCode, maxTimerLength, maxPlayers, startingLives) {
         this.roomCode = roomCode;
@@ -38,5 +39,13 @@ class Game {
 
         this.currentRound = 1;
         this.wordsSubmitted = {};
+    }
+
+    nextTurn() {
+
+    }
+
+    nextRound() {
+        
     }
 }
