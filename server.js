17009796f96be0c11bceaa992fc56c0b6d0fdd3b
Epic-Mushroom <malroop1@outlook.com>
2026-06-15 13:16:56 -0700

    restructure directories

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 const io = require("socket.io")(http);
 
 io.on("connection", (socket) => {
@@ -12,3 +16,5 @@ io.on("connection", (socket) => {
 });
 
 http.listen(3000, () => console.log("server up"));
+
+console.log("test");
